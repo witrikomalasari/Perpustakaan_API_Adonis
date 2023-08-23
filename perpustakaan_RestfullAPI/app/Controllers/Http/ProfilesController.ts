0028b174388c034b2454f93be96bd1da94129dec
witrikomalasari <dede.witri@gmail.com>
2023-08-23 21:55:56 +0700

    add validation(user, kategori, profile, buku, peminjaman)&prepare method crud in controllers folder

---
+++ perpustakaan_RestfullAPI/app/Controllers/Http/ProfilesController.ts
@@ -0,0 +1,13 @@
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+
+export default class ProfilesController {
+  public async store({}: HttpContextContract) {}
+
+  public async index({}: HttpContextContract) {}
+
+  public async show({}: HttpContextContract) {}
+
+  public async update({}: HttpContextContract) {}
+
+  public async destroy({}: HttpContextContract) {}
+}
